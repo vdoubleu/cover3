918bb8d7aa90d794f573d07284f00c0407e72d8c
vdoubleu <victorwang2001@gmail.com>
2023-10-29 16:30:42 -0400

    no cache

--- src/app/cover/sessions/page.tsx
+++ src/app/cover/sessions/page.tsx
@@ -14,7 +14,11 @@ export default function Page() {
 
   useEffect(() => {
 
-    fetch("/api/v1/sessions").then((response) => response.json())
+    fetch("/api/v1/sessions", {
+      next: {
+        revalidate: 1,
+      }
+    }).then((response) => response.json())
       .then((data) => setSessionData(
         data.map((session: any) => ({
           id: session.id,
